a45aa34cf4a7da8b887331be855bbd0dc6c77b72
MXMaddux <maxmaddux@gmail.com>
2025-07-12 06:02:11 -0700

    Section 3 vid 23

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,13 @@
-export const HomePage = () => {
-  return <>Prostore</>;
+import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
+  return (
+    <>
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+    </>
+  );
 };
 export default HomePage;
